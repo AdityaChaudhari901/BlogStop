4ae029c7f44171ac141eae32d9bedab64394d57f
Aditya Chaudhari <adityachaudhari901@gmail.com>
2025-03-10 12:23:35 +0530

    d

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -11,10 +11,11 @@ const Footer = () => {
         <Image src="/assets/logo_light.png" alt="BlogStop Logo" width={160} height={60} />
       </div>
 
-      {/* Copyright Text */}
-      <p className="text-white text-base w-full sm:w-auto mb-4 sm:mb-0">
-        © {year} BlogStop. All rights reserved.
-      </p>
+      {/* Text Section */}
+      <div className="text-white text-base font-normal w-full sm:w-auto mb-4 sm:mb-0">
+        <p>© {year} BlogStop. All rights reserved.</p>
+        <p className="mt-1">Created by Aditya Chaudhari</p>
+      </div>
 
       {/* Social Media Links */}
       <div className="flex gap-6">
